532cd98c2c9cde186945836ed5eedcc70581ea21
echiweshe <ernestchiweshe@gmail.com>
2021-03-25 05:59:34 +0200

    Added the following apps:     'blog',     'courses',     'pages',     'products'

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,10 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+# Tird Party
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Third party
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -38,6 +45,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    
+    # third party
+
+    # own
+    'blog',
+    'courses',
+    'pages',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +71,11 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 'DIRS': [],
+
+        # Third party
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+# Third party
+from pages.views import home_view, contact_view, about_view
+
 urlpatterns = [
     path('polls/', include('polls.urls')),
+
+    # Third party
+    path('blog/', include('blog.urls')),
+    path('courses/', include('courses.urls')),
+    path('products/', include('products.urls')),
+    path('', home_view, name='home'),
+    path('about/<int:id>/', about_view, name='product-detail'),
+    path('contact/', contact_view),
+
+    # Djabgo OG
     path('admin/', admin.site.urls),
 ]
+
